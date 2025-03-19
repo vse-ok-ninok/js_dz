d399ccc9dbf9135b462c9ea1264a4c48f3fc2dc1
nina <ninka867@yandex.ru>
2025-03-19 14:25:41 +0300

    Доп.изменения по дз№1

--- 6-driver/task3.js
+++ 6-driver/task3.js
@@ -1,7 +1,8 @@
 /*  Пользователь может вести машину только если ему больше 18 лет, он имеет права
 и он НЕ пьян*/
-let age = Number(prompt("Сколько тебе лет?"));
-let isDrunk = prompt("Вы трезвый? да/нет") == "нет" ? true : false;
-let hasLicense = prompt("У вас есть лицензия? да/нет") == "да" ? true : false;
+const age = Number(prompt("Сколько тебе лет?"));
+const isDrunk = prompt("Вы трезвый? да/нет") == "нет" ? true : false;
+const hasLicense = prompt("У вас есть лицензия? да/нет") == "да" ? true : false;
 
-console.log((age >=18) && !isDrunk && hasLicense ?"Может":"Не может");
+const canDrive = (age >=18) && !isDrunk && hasLicense;
+console.log(`Водитель ${canDrive ? 'может' : 'не может'} водить транспортное средство`);
